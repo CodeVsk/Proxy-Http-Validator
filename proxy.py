6085123ed839f982728c0d1fb59c0b7c669ca250
Vsk Code <vynycyusvieira@gmail.com>
2021-10-14 19:27:23 -0300

    First Commit Proxy

---
+++ proxy.py
@@ -0,0 +1,28 @@
+import requests, time, threading
+
+thread = 5
+proxys=None
+
+target = input("Link (http://example.com  or https://example.com): ")
+timeout = input("Timeout (5,10,15): ")
+
+r = requests.get("https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt")
+proxys = r.text.split('\n')
+
+def chunker_list(seq, size):
+    return (seq[i::size] for i in range(size))
+
+def recycler(proxylist,id_):
+    for x in proxylist:
+        try:
+            r = requests.get(target, proxies={"http":"http://"+x}, timeout=int(timeout))
+            if r.status_code == 200:
+                print("[PROXY ONLINE],http://"+x)
+        except:
+            pass
+
+data=list(chunker_list(proxys, thread))
+
+for i in range(0,thread):
+    t = threading.Thread(target=recycler, args = (data[i],i))
+    t.start()
